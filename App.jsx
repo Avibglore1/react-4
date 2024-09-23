11029499dbee68af612f117f15b81403b05c9025
Avinash <avinashkumarofficial95@gmail.com>
2024-09-23 18:21:02 +0530

    tailwind added

--- App.jsx
+++ App.jsx
@@ -1,31 +1,30 @@
+import './app.css'
 function App() {
 
   return (
     <>
-      {/* <header>
-        <div className="input-container">
-            <input type="text" name="" id=""
-             placeholder="Enter Location" 
-             className="search-input"
-             />
-            <button id="search" className="search-btn">Search</button>
+      <header className="h-[150px]  bg-[#2c3e50] flex justify-center items-center">
+        <div id="input-container" className="w-[60%] flex justify-between">
+            <input type="text" name="" id="" placeholder="Enter Location"
+            className="text-white text-lg outline-none py-[1rem] px-[0px] "/> 
+            <button id="search">Search</button>
         </div>
     </header>
     <main>
-        <div className="weather-container">
-            <div className="temprature">21.2°C</div>
-            <div className="location-date">
-                <div className="location">Delhi</div>
-                <span className="time">12:43</span>
-                <span className="Date">2024-07-15</span>
+        <div id="weather-container">
+            <div id="temprature">21.2°C</div>
+            <div id="location-date">
+                <div id="location">Delhi</div>
+                <span id="time">12:43</span>
+                <span id="Date">2024-07-15</span>
             </div>
-            <div className="weather-state">
-                <img src="//cdn.weatherapi.com/weather/64x64/day/113.png" className="emoji" alt=""/>
-                <div className="condition">Sunny</div>
+            <div id="weather-state">
+                <img src="//cdn.weatherapi.com/weather/64x64/day/113.png" id="emoji" alt=""/>
+                <div id="condition">Sunny</div>
             </div>
         </div>    
-    </main> */}
-    <h1 className="bg-black  text-white">Hello</h1>
+    </main> 
+    
     </>
   )
 }
