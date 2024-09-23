8e9de66c40bd99044d8d2306dbdb3ab5ff6156e6
Avinash <avinashkumarofficial95@gmail.com>
2024-09-23 17:07:35 +0530

    weather application using react

---
+++ App.jsx
@@ -0,0 +1,33 @@
+function App() {
+
+  return (
+    <>
+      {/* <header>
+        <div className="input-container">
+            <input type="text" name="" id=""
+             placeholder="Enter Location" 
+             className="search-input"
+             />
+            <button id="search" className="search-btn">Search</button>
+        </div>
+    </header>
+    <main>
+        <div className="weather-container">
+            <div className="temprature">21.2°C</div>
+            <div className="location-date">
+                <div className="location">Delhi</div>
+                <span className="time">12:43</span>
+                <span className="Date">2024-07-15</span>
+            </div>
+            <div className="weather-state">
+                <img src="//cdn.weatherapi.com/weather/64x64/day/113.png" className="emoji" alt=""/>
+                <div className="condition">Sunny</div>
+            </div>
+        </div>    
+    </main> */}
+    <h1 className="bg-black  text-white">Hello</h1>
+    </>
+  )
+}
+
+export default App
