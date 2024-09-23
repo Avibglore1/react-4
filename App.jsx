9e2924c23585cdc2a5ea1c7d01afa1f19e6f6946
Avinash <avinashkumarofficial95@gmail.com>
2024-09-23 19:27:52 +0530

    changes updated

--- App.jsx
+++ App.jsx
@@ -6,12 +6,14 @@ function App() {
       <header className="h-[150px]  bg-[#2c3e50] flex justify-center items-center">
         <div id="input-container" className="w-[60%] flex justify-between">
             <input type="text" name="" id="" placeholder="Enter Location"
-            className="text-white text-lg outline-none py-[1rem] px-[0px] "/> 
-            <button id="search">Search</button>
+            className="text-white text-lg outline-none py-[1rem] px-[0px] 
+            border-none border-b-2 border-white w-[84%]" /> 
+            <button id="search" className='bg-[#44ad96] text-lg text-white border-none
+            cursor-pointer py-[1rem] px-[2rem]'>Search</button>
         </div>
     </header>
-    <main>
-        <div id="weather-container">
+    <main className='h-[calc(100vh-150px)] flex items-center justify-center text-white bg-[#01161E]'> 
+        <div id="weather-container" className='flex items-center gap-[1rem] h-[5rem]'>
             <div id="temprature">21.2°C</div>
             <div id="location-date">
                 <div id="location">Delhi</div>
